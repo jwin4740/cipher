3308dae5c0da32669dd39a8e648d412615cb3b4a
James <jwin4740@gmail.com>
2016-12-28 09:38:52 -0800

    tv

--- assets/javascript/cipher.js
+++ assets/javascript/cipher.js
@@ -42,5 +42,35 @@
 
      });
 
+
+     $("#getmessage").on("click", function() {
+
+         messagi = $("#message").val();
+         $("#printmessage").append(" " + messagi);
+         console.log(messagi);
+
+
+
+
+         for (var i = 0, n = messagi.length; i < n; i++) {
+             console.log(messagi[i]);
+             console.log(messagi.charCodeAt(i));
+             if (messagi.charCodeAt(i) > 64 && messagi.charCodeAt(i) < 91) {
+                 decoded += (String.fromCharCode(((messagi.charCodeAt(i) - 65 + key) % 26 + 65)));
+             }
+             else if (messagi.charCodeAt(i) > 96 && messagi.charCodeAt(i) < 123) {
+                 decoded += (String.fromCharCode(((messagi.charCodeAt(i) - 97 + key) % 26 + 97)));
+             }
+             else
+             {
+                decoded += messagi[i];
+             }
+         }
+         console.log(messagi);
+         $("#printnumber3").append(decoded);
+
+
+
+     });
      //-------------------------
  });
